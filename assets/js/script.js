a18f0362136f147ba74e081d618c8621067d6af7
RozWelch <rozwelch@gmail.com>
2022-08-17 16:26:05 +0000

    Add form options on form page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("select-mini").onclick = function () {
+    location.href = "form.html";
+}
+
+document.getElementById("select-sports").onclick = function () {
+    location.href = "form.html";
+}
+
+document.getElementById("select-family").onclick = function () {
+    location.href = "form.html";
+}
