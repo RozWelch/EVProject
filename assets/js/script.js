dacc6ecbef11100247bdbe034c39be657db80db8
RozWelch <rozwelch@gmail.com>
2022-08-17 19:04:25 +0000

    Add circle image to form page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
 document.getElementById("select-mini").onclick = function () {
     location.href = "form.html";
+    
+    let carSelectedname = document.getElementById('selected-car-name');
+    carSelectedname.innerHTML = "<h3>Mini 2</h3>";
+
 }
 
 document.getElementById("select-sports").onclick = function () {
     location.href = "form.html";
+    
 }
 
 document.getElementById("select-family").onclick = function () {
     location.href = "form.html";
+    
 }
+
+
