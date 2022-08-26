50587c891f52d1b0207c80154fd9c9625bbcd480
RozWelch <rozwelch@gmail.com>
2022-08-26 09:09:55 +0000

    Update file name of form-mini to follow coding conventions

--- assets/js/homepage-script.js
+++ assets/js/homepage-script.js
@@ -1,12 +1,12 @@
 // link the circular car image to a form page
 document.getElementById("select-mini").onclick = function () {
-    location.href = "form_mini.html";
+    location.href = "form-mini.html";
 }
 
 document.getElementById("select-sports").onclick = function () {
-    location.href = "form_mini.html";    
+    location.href = "form-mini.html";    
 }
 
 document.getElementById("select-family").onclick = function () {
-    location.href = "form_mini.html";    
+    location.href = "form-mini.html";    
 }
