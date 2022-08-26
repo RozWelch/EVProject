22cfc730e1efdaceb55978613bea91b837490ff0
RozWelch <rozwelch@gmail.com>
2022-08-26 13:32:51 +0000

    Check JavaScript code through JSHint, and add in missing semicolons

--- assets/js/homepage-script.js
+++ assets/js/homepage-script.js
@@ -1,12 +1,12 @@
 // link the circular car image to a form page
-document.getElementById("select-mini").onclick = function () {
+document.getElementById("select-mini").onclick = function() {
     location.href = "form-mini.html";
-}
+};
 
-document.getElementById("select-sports").onclick = function () {
+document.getElementById("select-sports").onclick = function() {
     location.href = "form-mini.html";    
-}
+};
 
-document.getElementById("select-family").onclick = function () {
+document.getElementById("select-family").onclick = function() {
     location.href = "form-mini.html";    
-}
+};
