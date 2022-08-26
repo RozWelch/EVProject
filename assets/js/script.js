a111ff9b69fe2f57c7ef204ae022da6f6cc69c5e
RozWelch <rozwelch@gmail.com>
2022-08-26 13:33:30 +0000

    Check JavaScript code through JSHint, and add in missing semicolons

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,7 +50,7 @@ btn.onclick = function (calculate_range) {
   document.getElementById("display_results").innerHTML = `Your current range is ${newRange} km`;
 
   // check where the final destination is, and the diplay journey distance
-  let journeyDist
+  let journeyDist;
   if (finalDestination === '1') {
     journeyDist = 203;
     document.getElementById("display_journeydistance").innerHTML = `Your journey distance to Limerick is ${journeyDist}km`;
@@ -71,16 +71,16 @@ btn.onclick = function (calculate_range) {
     document.getElementById("display_rechargemessage").innerHTML = `Your journey distance is within range. Below is a map of charging points at your destination`;
   }
 
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal it closes
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
